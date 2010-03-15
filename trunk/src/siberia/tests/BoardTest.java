30e62dcbb456d23c645dd82d48b6ff3cb0f673c2
henry.blum <henry.blum@d561ccc0-0c76-11df-aac2-bbd07866f500>
2010-03-15 06:07:13 +0000

    Bishop, BoardTest and ChessApplet test cases. Code coverage actually creating reports with CodeCover now. CodeCover html reports in codecover_reports directory

--- trunk/src/siberia/tests/BoardTest.java
+++ trunk/src/siberia/tests/BoardTest.java
@@ -6,10 +6,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import siberia.Board;
+import siberia.BoardLocation;
+import siberia.ChessApplet;
+import siberia.DragBoard;
+import siberia.PlayerController;
+
 public class BoardTest {
 
+	private Board subject;
+	private PlayerController pc;
+	private BoardLocation location;
+	private boolean white;
+	
 	@Before
 	public void setUp() throws Exception {
+		pc = new PlayerController();
+		white = true;
+		subject = new Board(pc, white);
 	}
 
 	@After
@@ -18,27 +32,38 @@ public class BoardTest {
 
 	@Test
 	public final void testBoard() {
-		fail("Not yet implemented"); // TODO
+		assertNotNull(subject);
+		subject = new Board(pc, !white);
+		assertNotNull(subject);
 	}
 
 	@Test
 	public final void testGetLocationIntInt() {
-		fail("Not yet implemented"); // TODO
+		location = subject.getLocation(-1, 0);
+		assertNull(location);
+		location = subject.getLocation(8, 0);
+		assertNull(location);
+		location = subject.getLocation(0, -1);
+		assertNull(location);
+		location = subject.getLocation(0, 8);
+		assertNull(location);
+		location = subject.getLocation(3, 3);
+		assertNotNull(location);
 	}
 
 	@Test
 	public final void testSetDragBoard() {
-		fail("Not yet implemented"); // TODO
+		ChessApplet applet = new ChessApplet();
+		DragBoard db = new DragBoard(subject, applet, pc);
+		subject.setDragBoard(db);
+		assert(true);
 	}
 
 	@Test
 	public final void testUpdateBoolean() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testPaintComponentGraphics() {
-		fail("Not yet implemented"); // TODO
+		subject.update(white);
+		subject.update(!white);
+		assert(true);
 	}
 
 }
