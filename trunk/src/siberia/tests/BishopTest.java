d0fe5a665c0d508ee683a5df463812d490ae4ef4
henry.blum <henry.blum@d561ccc0-0c76-11df-aac2-bbd07866f500>
2010-03-15 17:41:12 +0000

    BishopTest now covering most of the Bishop class. Need help with creating board configurations and moves covering all conditions in the verifyMove methods of pieces

--- trunk/src/siberia/tests/BishopTest.java
+++ trunk/src/siberia/tests/BishopTest.java
@@ -11,10 +11,40 @@ import siberia.pieces.Bishop;
 public class BishopTest {
 	
 	private Bishop subject;
-
+	private int[][] board_at_start;
+	private int[][] board_in_game;
+	
 	@Before
 	public void setUp() throws Exception {
 		subject = new Bishop("white", "tester");
+		
+		board_at_start = new int[8][8];
+		
+		board_at_start[0] = new int[]{10,12,13,9,8,13,12,11};
+		board_at_start[1] = new int[]{14,14,14,14,14,14,14,14};
+		
+		// 0 corresponds to empty space
+		for(int i = 2; i <= 5; i++){
+			for(int j = 0; j < board_at_start[i].length; j++){
+				board_at_start[i][j] = 0;			
+			}
+		}
+		board_at_start[6] = new int[]{7,7,7,7,7,7,7,7};
+		board_at_start[7] = new int[]{3,5,6,2,1,6,5,4};
+		
+		board_in_game = new int[8][8];
+		
+		board_in_game[0] = new int[]{10,12,13,9,8,13,12,11};
+		board_in_game[1] = new int[]{14,14,14,14,14,14,14,14};
+		
+		// 0 corresponds to empty space
+		for(int i = 2; i <= 5; i++){
+			for(int j = 0; j < board_in_game[i].length; j++){
+				board_in_game[i][j] = 0;			
+			}
+		}
+		board_in_game[6] = new int[]{7,0,7,7,7,7,7,7};
+		board_in_game[7] = new int[]{3,5,6,2,1,6,5,4};
 	}
 
 	@After
@@ -22,21 +52,30 @@ public class BishopTest {
 	}
 
 	@Test
-	public final void testVerify_move_valid() {
-		int[][] board = {};
-		int[] position = {7, 1};
-		int[] next = {5, 2};
+	public final void testVerify_move_invalid_jumping() {
 		
-		assertTrue(subject.verify_move(board, position, next));
+		int[] position = {7, 2};
+		int[] next = {5, 0};
+		
+		assertFalse(subject.verify_move(board_at_start, position, next));
 	}
-
+	
 	@Test
-	public final void testVerify_move_invalid() {
-		int[][] board = {};
-		int[] position = {1,1};
-		int[] next = {-1, -1};
+	public final void testVerify_move_invalid_attacking_own_piece() {
+
+		int[] position = {7, 2};
+		int[] next = {6, 4};
 		
-		assertFalse(subject.verify_move(board, position, next));
+		assertFalse(subject.verify_move(board_at_start, position, next));
+	}
+	
+	@Test
+	public final void testVerify_move_valid() {
+
+		int[] position = {7, 2};
+		int[] next = {5, 0};
+		
+		assertTrue(subject.verify_move(board_in_game, position, next));
 	}
 
 	
