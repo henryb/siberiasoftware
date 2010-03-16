49fc24350fb360ec0bd6244d3cfea90d91859f5f
henry.blum <henry.blum@d561ccc0-0c76-11df-aac2-bbd07866f500>
2010-03-16 04:16:41 +0000

    Updated ChessAppletTest to reflect JFrame instead of JApplet Removed applet methods from ChessApplet

--- src/siberia/tests/ChessAppletTest.java
+++ src/siberia/tests/ChessAppletTest.java
@@ -24,21 +24,13 @@ public class ChessAppletTest {
 	}
 
 	@Test
-	public final void testInit() {
+	public final void testChessApplet() {
 		assertNotNull(subject);
-		subject.init();
-		assert(true);
-	}
-
-	@Test
-	public final void testDestroy() {
-		subject.destroy();
 		assert(true);
 	}
 
 	@Test
 	public final void testCheckMove() {
-		subject.init();
 		move = ""; //garbled move string
 		subject.checkMove(move);
 		assert(true);
@@ -46,7 +38,6 @@ public class ChessAppletTest {
 
 	@Test
 	public final void testCheckMove_invalid() {
-		subject.init();
 		move = "0 0 7 7"; //invalid move
 		subject.checkMove(move);
 		assert(true);
@@ -54,7 +45,6 @@ public class ChessAppletTest {
 	
 	@Test
 	public final void testCheckMove_valid() {
-		subject.init();
 		move = "7 1 5 2"; //valid move
 		subject.checkMove(move);
 		assert(true);
@@ -62,7 +52,6 @@ public class ChessAppletTest {
 	
 	@Test
 	public final void testPutMove_gameover() {
-		subject.init();
 		move = "1 1 1 1";
 		subject.putMove(move);
 		assert(true);
@@ -70,7 +59,6 @@ public class ChessAppletTest {
 	
 	@Test
 	public final void testPutMove() {
-		subject.init();
 		move = "2 2 3 3";
 		subject.putMove(move);
 		assert(true);
